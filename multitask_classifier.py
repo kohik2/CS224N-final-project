893f21f3a4974e085b4c0e6183f7ca032ad2d324
samprietoserrano <94136254+samprietoserrano@users.noreply.github.com>
2024-03-09 17:16:19 -0800

    working on dimensions

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -241,7 +241,7 @@ def train_multitask(args):
             logits = model.predict_similarity(b_ids1, b_mask1, b_ids2, b_mask2)
             target = b_labels.view(-1)
 
-            # logits = logits.to(device)
+            logits = logits.to(device)
             # target = target.to(device)
             logits = logits.view(args.batch_size, 1)
 
