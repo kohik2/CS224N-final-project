39cd8fd5129652b861a33981c26606d2df380908
samprietoserrano <94136254+samprietoserrano@users.noreply.github.com>
2024-03-13 00:44:06 -0700

    fix error?

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -177,8 +177,6 @@ def train_multitask(args):
                                         collate_fn=sts_dev_data.collate_fn)
     
     # para set 
-    para_train_data = random.sample(para_train_data, 8500)
-    para_train_data = SentencePairDataset(para_train_data, args)
     para_train_data_sampled = random.sample(para_train_data, 8500)
     para_train_data = SentencePairDataset(para_train_data_sampled, args)
     para_dev_data = SentencePairDataset(para_dev_data, args)
