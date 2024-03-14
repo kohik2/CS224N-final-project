8b50a9f6bc1983658010c26db67886fd7015d45d
samprietoserrano <94136254+samprietoserrano@users.noreply.github.com>
2024-03-13 17:08:37 -0700

    todevice again

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -282,7 +282,7 @@ def train_multitask(args):
             b_mask2 = b_mask2.to(device)
 
             optimizer.zero_grad()
-            logits = model.predict_paraphrase(b_ids1, b_mask1, b_ids2, b_mask2)
+            logits = model.predict_paraphrase(b_ids1, b_mask1, b_ids2, b_mask2).to(device)
 
             # # Hand-coding the MNSR loss calculation below.
             # # Generate negative indices
@@ -305,7 +305,7 @@ def train_multitask(args):
             # target = torch.ones_like(negative_scores)  # Positive pair
             # loss = mnsr_loss(positive_scores, negative_scores, target)
             loss = F.binary_cross_entropy_with_logits(input=logits, target=b_labels.view(-1).float().to(device), reduction='sum') / args.batch_size
-            # loss = torch.autograd.Variable(loss, requires_grad=True) # https://discuss.pytorch.org/t/runtimeerror-element-0-of-variables-does-not-require-grad-and-does-not-have-a-grad-fn/11074
+            loss = torch.autograd.Variable(loss, requires_grad=True) # https://discuss.pytorch.org/t/runtimeerror-element-0-of-variables-does-not-require-grad-and-does-not-have-a-grad-fn/11074
 
             pooled_rep_1 = model.forward(input_ids=b_ids1, attention_mask=b_mask1).to(device)
             pooled_rep_2 = model.forward(input_ids=b_ids2, attention_mask=b_mask2).to(device)
